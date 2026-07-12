02b5cf07baa8ae11a2d8f7ffcba9cefcd4c1c532
TNR <nyrinaratovo@gmail.com>
2026-07-12 19:04:00 +0000

    Initialisation projet: creation du fichier expliquant la modelisation et le choix des strategies d'heritage JPA

---
+++ src/test/java/ny/rina/test_tech/TestTechApplicationTests.java
@@ -0,0 +1,13 @@
+package ny.rina.test_tech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
